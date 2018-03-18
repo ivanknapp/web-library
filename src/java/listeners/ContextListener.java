9a2a91ddcd4a8dcdbe30574c9a20509eab7c11e9
ivanknapp <ivanknapp@yandex.ru>
2018-03-18 15:19:04 +0400

    added listeners context, httpsession

---
+++ src/java/listeners/ContextListener.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listeners;
+
+import java.util.HashMap;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Web application lifecycle listener.
+ *
+ * @author Иван
+ */
+public class ContextListener implements ServletContextListener {
+
+    private HashMap<String, HttpSession> sessionMap = new HashMap();
+    
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        sce.getServletContext().setAttribute("sessionMap", sessionMap);
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+        sce.getServletContext().removeAttribute("sessionMap");
+        //for GC
+        sessionMap = null;
+    }
+}
